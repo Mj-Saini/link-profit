6faff50c8fefb6da69a7f13d5ce8a7cb5bbe5d37
mj-saini <manojsaini97285@gmail.com>
2025-09-19 14:00:54 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/registration',
+        permanent: true, // browser caching ke liye
+      },
+    ];
+  },
+};
 
 export default nextConfig;
