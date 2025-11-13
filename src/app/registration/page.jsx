919934ecd25e7406040ebdb4cb9e06b20b8e68bb
mj-saini <manojsaini97285@gmail.com>
2025-11-13 10:23:52 +0530

    remove console

--- src/app/registration/page.jsx
+++ src/app/registration/page.jsx
@@ -1,26 +1,9 @@
 
-// // /app/registration/page.jsx
-// import React, { Suspense } from "react";
-// import RegistrationForm from "../components/RegistrationForm";
-
-// export default function Page() {
-//     return (
-//         <Suspense fallback={<div>Loading...</div>}>
-//             <RegistrationForm />
-//         </Suspense>
-//     );
-// }
-
-
 "use client";
-import { Suspense, useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { onAuthStateChanged } from "firebase/auth";
+import { Suspense } from "react";
 import RegistrationForm from "../components/RegistrationForm";
-import { auth } from "../lib/firebaseConfig";
 
 export default function RegistrationPage() {
-    const router = useRouter();
 
   
 
